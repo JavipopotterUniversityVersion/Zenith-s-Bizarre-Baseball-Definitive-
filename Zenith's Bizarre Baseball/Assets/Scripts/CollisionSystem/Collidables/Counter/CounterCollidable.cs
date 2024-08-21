988c6e96f0036d1b3ab1d692b0fdb43cab473908
Javipopotter <javivals@ucm.es>
2024-08-21 02:43:00 +0200

    fully implemented batter

--- Zenith's Bizarre Baseball/Assets/Scripts/CollisionSystem/Collidables/Counter/CounterCollidable.cs
+++ Zenith's Bizarre Baseball/Assets/Scripts/CollisionSystem/Collidables/Counter/CounterCollidable.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MyBox;
 using UnityEngine;
 
 public class CounterCollidable : ICollidable
 {
     [SerializeField] Float _speedAdder;
+
+    [SerializeField] bool _hasMaxSpeed = false;
+    
+    [ConditionalField(nameof(_hasMaxSpeed), false)] 
+    [SerializeField] float _maxSpeed = 0;
 
     public override void OnCollide(Collider2D collider)
     {
@@ -16,6 +22,7 @@ public class CounterCollidable : ICollidable
             if(rigidbody.velocity.magnitude < _speedAdder.Value * knockable.Reduction) force = _speedAdder.Value;
             else force = rigidbody.velocity.magnitude + (_speedAdder.Value * 0.5f);
 
+            force = _hasMaxSpeed ? Mathf.Min(force, _maxSpeed) : force;
             knockable.Knock(force * (collider.transform.position - transform.position).normalized);
         }
     }

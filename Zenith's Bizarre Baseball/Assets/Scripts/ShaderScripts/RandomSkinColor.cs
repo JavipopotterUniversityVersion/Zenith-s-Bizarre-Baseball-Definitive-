468e326ceccbfdabc4b378578744037a0d59372b
Javipopotter <javivals@ucm.es>
2024-08-23 20:45:11 +0200

    improved skin color change shader

--- Zenith's Bizarre Baseball/Assets/Scripts/ShaderScripts/RandomSkinColor.cs
+++ Zenith's Bizarre Baseball/Assets/Scripts/ShaderScripts/RandomSkinColor.cs
@@ -7,8 +7,8 @@ public class RandomSkinColor : MonoBehaviour
     private void Awake() {
         MaterialPropertyBlock block = new MaterialPropertyBlock();
 
-        block.SetFloat("_num", Random.Range(0f,1f));
-        block.SetFloat("_num3", Random.Range(0f,1f));
+        block.SetFloat("_SkinNum", Random.Range(0f,1f));
+        block.SetFloat("_HairNum", Random.Range(0f,1f));
 
         GetComponent<Renderer>().SetPropertyBlock(block);
     }

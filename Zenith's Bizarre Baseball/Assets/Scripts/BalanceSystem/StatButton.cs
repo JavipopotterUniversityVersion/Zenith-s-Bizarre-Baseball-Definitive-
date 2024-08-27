67b6a5507430068545226d87da6f85c4501a9921
Javipopotter <javivals@ucm.es>
2024-08-27 14:47:58 +0200

    Before Balanceable Stats

---
+++ Zenith's Bizarre Baseball/Assets/Scripts/BalanceSystem/StatButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatButton : MonoBehaviour
+{
+    // [SerializeField] Int
+    [SerializeField] string _statName;
+    [SerializeField] int _statCost;
+    [SerializeField] TextMeshProUGUI _statNameText;
+    [SerializeField] Button _substractButton;
+    [SerializeField] Button _addButton;
+
+
+}
--- Zenith's Bizarre Baseball/Assets/Scripts/StateSystem/StateMachine/Behaviour/InstanceBehaviours/InstanceBlockSystem/RandomInstanceOnBlocks.cs
+++ Zenith's Bizarre Baseball/Assets/Scripts/StateSystem/StateMachine/Behaviour/InstanceBehaviours/InstanceBlockSystem/RandomInstanceOnBlocks.cs
@@ -2,15 +2,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class RandomInstanceOnBlocks : MonoBehaviour, IBehaviour
 {
-    [SerializeField] InstanceUnit[] globalInstanceUnits;
+    [SerializeField] InstanceGroup[] instanceGroups;
+    [SerializeField] InstanceUnit[] UniversalUnits;
     InstanceBlock[] globalInstanceBlocks;
+    InstanceUnit[] selectedUnits;
 
     private void Awake() => globalInstanceBlocks = GetComponentsInChildren<InstanceBlock>(true);
+    private void Start()
+    {
+        selectedUnits = new InstanceUnit[0];
+        selectedUnits = selectedUnits.Concat(UniversalUnits).ToArray();
+        selectedUnits = selectedUnits.Concat(InstanceGroup.GetRandomGroup(instanceGroups).instanceUnits).ToArray();
+    }
+
     public void ExecuteBehaviour()
     {
-        globalInstanceBlocks[UnityEngine.Random.Range(0,globalInstanceBlocks.Length)].Instance(globalInstanceUnits);
+        globalInstanceBlocks[UnityEngine.Random.Range(0,globalInstanceBlocks.Length)].Instance(selectedUnits);
+    }
+
+    [Serializable]
+    class InstanceGroup
+    {
+        public InstanceUnit[] instanceUnits;
+        [Range(0,1)] public float probability;
+
+        public static InstanceGroup GetRandomGroup(InstanceGroup[] groups)
+        {
+            float totalProbability = groups.Sum(group => group.probability);
+            float randomValue = UnityEngine.Random.Range(0, totalProbability);
+            float currentProbability = 0;
+
+            foreach (var group in groups)
+            {
+                currentProbability += group.probability;
+                if (randomValue <= currentProbability) return group;
+            }
+
+            return null;
+        }
     }
 }
